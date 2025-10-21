ddd9498821cf17d4573a46dfbec1f358864d8bd2
Nathan Santos <nathan.oliver07@hotmail.com>
2025-10-21 20:08:46 -0300

    Delete hotel directory

--- hotel/class/local/Reserva.js
+++
@@ -1,9 +0,0 @@
-export class Reserva{
-    constructor(quarto, data, cliente, hoje){
-        this.dataDeCriacao = hoje
-        this.quarto = quarto
-        this.data = data
-        this.cliente = cliente
-    }
-    
-}
